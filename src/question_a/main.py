ea641c7b25c1deb9ea70f01703d4c770e171edcf
JoowonKim3642 <joowon.kim@g.austincc.edu>
2024-12-14 04:17:35 +0000

    Q1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,36 @@
-#add import
+#Q1 main
+
+import question_a
+
+def main():
+     
+    while True:
+
+        print("MENU\n1-multiplication table\n2-Exit")
+        option = int(input("Select 1 or 2: "))
+
+        if option == 1:
+            num1 = int(input("please enter the number for row: ")) #enter 10
+            num2 = int(input("please enter the number for column: ")) #enter 5
+            list = question_a.create_multiplication_table(num1, num2)
+            question_a.display_multiplication_table(list, num1)
+
+            while True:
+                choice = input("Do you want to continue? y/n ").upper()
+
+                if choice == 'Y':
+                    break
+                elif choice == 'N':
+                    print("Exit")
+                    exit()
+                else:
+                    print("Invalid value")
+                    
+        elif option == 2:
+            print("Exit")
+            break
+            
+        else:
+            print("Invalid choice. Select 1 or 2")
+
+main()
