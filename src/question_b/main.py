f2fcd471567ab6ed8cd5183b52c3c85378a69812
JoowonKim3642 <joowon.kim@g.austincc.edu>
2024-12-14 02:59:10 +0000

    Q2

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,23 @@
-#add import
+#Q2 main
+
+import question_b
+
+def main():
+    while True:
+        print("MENU\n1-Display stock purchase history\n2-Exit")
+        option = int(input("Select 1 or 2: "))
+
+        if option == 1:
+            stock_list = question_b.get_stock_list()
+
+            for stock in stock_list:
+                print(stock.get_symbol() + " " + stock.get_company_name())
+
+        elif option == 2:
+            print("Exit")
+            break
+            
+        else:
+            print("Invalid choice. Select 1 or 2")
+
+main()
